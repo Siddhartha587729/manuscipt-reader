200cdc59c2c94e91ea7579fbe60b1a9e853510b0
Siddhartha587729 <Siddhartha587729@gmail.com>
2023-12-17 14:11:05 +0530

    preview tiles added

--- src/Components/Preview.jsx
+++ src/Components/Preview.jsx
@@ -1,20 +1,31 @@
 import React from 'react'
-import UploadForm from './UploadForm'
 
-const Tile= ({fr}) =>{
-    return (
-        <div className='h-10 w-10 bg-slate-900 border-2 border-black'>
-            <p>{fr}</p>
-        </div>
-    )
-}
+const Tile = ({ fr }) => {
+  return (
+    <div className='relative group h-52 w-full sm:w-[200px] md:w-[210px] lg:w-[250px] xl:w-[250px] bg-slate-200 border-2 border-black overflow-hidden'>
+      {/* Invisible div */}
+      <div className='bg-gray-600 w-full h-52 flex justify-center items-center absolute opacity-0 group-hover:opacity-50 transition-opacity'>
+        <h1 className='text-white'>fhf</h1>
+      </div>
+
+      {/* Visible div */}
+      <div className='relative z-10'>
+        <p className='text-center'>{fr}</p>
+      </div>
+    </div>
+  );
+};
+
+
 
 const Preview = () => {
-    const arr =['apple','mango','grapes']
+    const arr =['apple','mango','grapes','ghuiyan']
   return (
     <>
-        <h1>upload</h1>
-        <div>
+        <div className='my-6 flex justify-center'>
+            <h1 className='text-3xl font-medium text-[#15133C]'><span className='text-orange-600 font-extrabold'>P</span>review</h1>
+        </div>
+        <div className='flex flex-wrap gap-10 my-10 px-10 '>
             {arr.map((fruit,index)=>{
                 console.log(fruit);
                 return <Tile key={index} fr={fruit}/>

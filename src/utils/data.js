17ececdd10b129696535958d6127d3d164e19309
Sahil Kumar <sahilku601@gmail.com>
2023-12-22 01:54:56 +0530

    Display Excel Fetching

--- src/utils/data.js
+++ src/utils/data.js
@@ -11,3 +11,47 @@ export const fetchuser =`*[_type=='author'] | order(_createdAt,desc){
     title,
     username
 }`
+export const fetchUserById = (id)=>{
+    const query = `*[_type=='author' && _id=='${id}']{
+        _createdAt,
+        _id,
+        author,
+        coverImage{
+            asset->{
+                url
+            }   
+        },
+        organisation,
+        title,
+        username
+    }`
+    return query;
+}
+export const fetchData = (id) =>{
+    const query = `*[_type=='script' && author._ref=='${id}']{
+        hole_position,
+        classId,
+        end,
+        noOfPlamLeaves,
+        CPD,
+        pageNo,
+        _createdAt,
+        details,
+        paints,
+        coloPhone,
+        line,
+        title,
+        density,
+        language,
+        letters,
+        folio_number,
+        notes,
+        dimmentions,
+        genre,
+        condition,
+        begining,
+        size,
+        remarks,
+    }`
+    return query;
+} 

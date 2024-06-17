5ea7598ebbe58678c1bd6f5582f0094bfecf3667
Sahil Kumar <sahilku601@gmail.com>
2024-06-17 23:45:38 +0530

    [fix] new team member image added

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -16,9 +16,9 @@ const Footer = () => {
             <div className="sm:flex sm:items-center sm:justify-between">
               <Link to="#" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
                 <FaScroll size={40} color="white"/>
-                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Manuscript Reader</span>
+                <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">Manuscript Reader</span>
               </Link>
-              <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+              <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-400 sm:mb-0">
                 <li>
                   <Link to="/team" className="hover:underline me-4 md:me-6">About</Link>
                 </li>
@@ -29,8 +29,8 @@ const Footer = () => {
               </ul>
             </div>
 
-            <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-            <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <Link to="#" className="hover:underline">Manuscript Reader</Link>. Design by KIIT University Student's in collaboration with  Otani university, Japan.</span>
+            <hr className="my-6 border-gray-700 sm:mx-auto lg:my-8" />
+            <span className="block text-sm text-gray-400 sm:text-center">© 2023 <Link to="#" className="hover:underline">Manuscript Reader</Link>. Design by KIIT University Student's in collaboration with  Otani university, Japan.</span>
           </div>
         </footer>
 
